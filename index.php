3b7bdc443d77c066898276446db8ff22b56eb833
Chris Wheeler <grintor@gmail.com>
2014-12-08 20:57:51 -0500

    little clean up and documentation

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <?php
 	$bcGUID   = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx";
 	$bcPWD    = "xxxxxxxxxxx";
-	$BTCprice = json_decode(file_get_contents("https://blockchain.info/ticker"), true);
 ?>
-
 <html>
 	<body>
 		<?php
-			if (!$_GET) {
+			if (!$_GET) {	// if there are no GET variables, they are landing here directly so show them the homepage
 				print('
 					Choose which to buy:
 					<br />
@@ -18,32 +16,36 @@
 					<a href="?choice=3">Katy Perry - Roar</a>
 				');
 			}
-			if ($_GET["choice"] == "1") {
+			if ($_GET["choice"]) {																	// If they made any selection, 
+				$BTCprice = json_decode(file_get_contents("https://blockchain.info/ticker"), true);	// you need to know the btc price.
+			}
+			if ($_GET["choice"] == "1") {	// user selection 1
 				$USDprice = 0.15;
 				$BTCprice = round($USDprice / $BTCprice["USD"]["last"] * 1000000);
 				print("Miley Cyrus - Wrecking Ball costs $$USDprice ($BTCprice bits) <br />");
 			}
-			if ($_GET["choice"] == "2") {
+			if ($_GET["choice"] == "2") {	// user selection 2
 				$USDprice = 0.25;
 				$BTCprice = round($USDprice / $BTCprice["USD"]["last"] * 1000000);
 				print("PSY - GANGNAM STYLE costs $$USDprice ($BTCprice bits) <br />");
 			}
-			if ($_GET["choice"] == "3") {
+			if ($_GET["choice"] == "3") {	// user selection 3
 				$USDprice = 0.35;
 				$BTCprice = round($USDprice / $BTCprice["USD"]["last"] * 1000000);
 				print("Katy Perry - Roar costs $$USDprice ($BTCprice bits) <br />");
 			}
-			
 			if ($_GET["choice"]) {
 				$satoshi = $BTCprice * 100;
-				$expires = time() + 3600;
+				$expires = time() + 3600;	// make the generated url only valid for an hour
 				$product = $_GET["choice"];
-				$BTCaddress = json_decode(file_get_contents("https://blockchain.info/merchant/$bcGUID/new_address?password=$bcPWD"), true);
+				$BTCaddress = json_decode(file_get_contents("https://blockchain.info/merchant/$bcGUID/new_address?password=$bcPWD"), true);		// we request a new bitcoin address for our wallet
 				$BTCaddress = $BTCaddress[address];
-				$hamc = hash_hmac('sha256', "$expires-$product-$BTCaddress-$satoshi" , $bcPWD);
-				$url = "?paid=true&expires=$expires&BTCaddress=$BTCaddress&satoshi=$satoshi&product=$product&hmac=$hamc";
+				$hamc = hash_hmac('sha256', "$expires-$product-$BTCaddress-$satoshi" , $bcPWD);	// to make sure the URL is not manipulated
+				$url = "?paid=true&expires=$expires&BTCaddress=$BTCaddress&satoshi=$satoshi&product=$product&hmac=$hamc";	// this URL has all the info we want to verify in it and is secured by HMAC
 				print("Send this ammount to $BTCaddress <br />");
 				print("Do not refresh or leave this page! <br />");
+				// below we make an iframe where the current balance of the address we generated above is displayed (but the iframe is hidden)
+				// a javascript reads the contents of the iframe and when the contents show that the balance is >= the item price, it redirects the page to the product page.
 				print('
 					<iframe width="0" height="0" style="visibility:hidden;display:none" id="iframe0" src="?addressbalance='.$BTCaddress.'"></iframe>
 					<script>
@@ -57,7 +59,7 @@
 					</script>
 				');
 			}
-			if ($_GET["addressbalance"]){
+			if ($_GET["addressbalance"]){	// this is a workaround for the fact that you can't read the contents of an iframe from an external domain using javascript
 				print(file_get_contents("https://blockchain.info/q/addressbalance/".$_GET["addressbalance"]."?confirmations=0"));
 			}
 			if ($_GET["paid"]){
@@ -66,13 +68,13 @@
 				$satoshi = $_GET["satoshi"];
 				$product = $_GET['product'];
 				$hamc = $_GET["hmac"];
-				if (time() > $expires) {
+				if (time() > $expires) {	// check that the URL is not expired. Remember, they can't manupulate the expire time in the URL because of the HMAC
 					print("This URL has expired!");
 				} else {
-					if($hamc != hash_hmac('sha256', "$expires-$product-$BTCaddress-$satoshi" , $bcPWD)) {
+					if($hamc != hash_hmac('sha256', "$expires-$product-$BTCaddress-$satoshi" , $bcPWD)) {	// make sure the URL is not manipulated
 						print("Invalid URL!");
 					} else {
-						if(intval(file_get_contents("https://blockchain.info/q/addressbalance/$BTCaddress?confirmations=0")) >= $satoshi) {
+						if(intval(file_get_contents("https://blockchain.info/q/addressbalance/$BTCaddress?confirmations=0")) >= $satoshi) {	// make sure they actually paid
 							if ($product == "1") {
 								print ('<iframe width="560" height="315" src="//www.youtube.com/embed/My2FRPA3Gf8?list=PLirAqAtl_h2r5g8xGajEwdXd3x1sZh8hC" frameborder="0" allowfullscreen></iframe>');
 							}
@@ -83,7 +85,7 @@
 								print ('<iframe width="560" height="315" src="//www.youtube.com/embed/CevxZvSJLk8?list=PLirAqAtl_h2r5g8xGajEwdXd3x1sZh8hC" frameborder="0" allowfullscreen></iframe>');
 							}
 						} else {
-							print("Stop that!");
+							print("Stop that!");	// they copied and pasted the URL from the page source and tried to navigate to it without paying
 						}
 					}
 				}
